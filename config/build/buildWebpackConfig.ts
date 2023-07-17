59ea9cb1487eb18fb53055608f6e1053b238731f
Dmitry Petrov <53710984+dmsk01@users.noreply.github.com>
2023-07-17 22:50:02 +0300

    add styles to config

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,32 +1,27 @@
-import { Configuration } from "webpack";
 import { BuildOptions } from "./types/config";
-import path from "path";
-import { buildPlugings } from "./buildPlugings";
+import webpack from "webpack";
+import { buildPlugins } from "./buildPlugins";
+import { buildLoaders } from "./buildLoaders";
 import { buildResolvers } from "./buildResolvers";
 import { buildDevServer } from "./buildDevServer";
 
-export function buildWebpackConfig(options: BuildOptions): Configuration {
-  const { paths, mode, isDev } = options;
-  return {
-    mode,
-    entry: paths.entry,
-    output: {
-      filename: '[name].[contenthash].js',
-      path: paths.build,
-      clean: true,
-    },
-    plugins: buildPlugings(options),
-    module: {
-      rules: [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
+export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
+    const { paths, mode, isDev } = options;
+
+    return {
+        mode: mode,
+        entry: paths.entry,
+        output: {
+            filename: "[name].[contenthash].js",
+            path: paths.build,
+            clean: true
         },
-      ],
-    },
-    resolve: buildResolvers(),
-    devtool: isDev ? 'inline-source-map' : undefined,
-    devServer: isDev ? buildDevServer(options) : undefined,
-  }
+        plugins: buildPlugins(options),
+        module: {
+            rules: buildLoaders(),
+        },
+        resolve: buildResolvers(),
+        devtool: isDev ? 'inline-source-map' : undefined,
+        devServer: isDev ? buildDevServer(options) : undefined,
+    }
 }
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { someFn } from "./test";
-
-console.log(someFn(777));
-
-document.body.innerHTML = '<h1>hello world</h1>'
