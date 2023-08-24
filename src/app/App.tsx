4e203ef773c7745108301499d050d090823c7b08
Dmitry Petrov <53710984+dmsk01@users.noreply.github.com>
2023-08-24 22:27:48 +0300

    add feature sliced structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,11 @@
-import React, { Suspense, useContext, useState } from "react";
+import React, { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
 import "./styles/index.scss";
-import { Theme, ThemeContext } from "./theme/ThemeContext";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
 
 function App() {
   const { theme, toggleTheme } = useTheme();
@@ -20,8 +19,8 @@ function App() {
       <Link to={"/about"}>About</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path={"/about"} element={<AboutPageAsync />} />
-          <Route path={"/"} element={<MainPageAsync />} />
+          <Route path={"/about"} element={<AboutPage />} />
+          <Route path={"/"} element={<MainPage />} />
         </Routes>
       </Suspense>
     </div>
